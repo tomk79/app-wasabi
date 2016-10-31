6b91b2406fca7cd39edf1b8d563464777ebc13ae
Tomoya Koyanagi <tomk79@gmail.com>
2016-10-31 16:54:24 +0900

    id は where しなくても find でいいみたい

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -94,7 +94,7 @@ class ProjectController extends Controller
         // var_dump('---- update('.$id.') ----');
         $data = $request->all();
         $this->project
-            ->where('id',$id)
+            ->find($id)
             ->update(array(
                 'name'=>$data['name'],
                 'account'=>$data['account'],
