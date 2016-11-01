7e0c6f6c63b7faf64b9decc0614dfd5c3251a777
Tomoya Koyanagi <tomk79@gmail.com>
2016-11-01 18:09:02 +0900

    relay_users_x_projects に改名

--- app/relayUsersXProjects.php
+++ app/relayUsersXProjects.php
@@ -4,14 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class relayUserXProjects extends Model
+class relayUsersXProjects extends Model
 {
     /**
      * The table associated with the model.
      *
      * @var string
      */
-    protected $table = 'relay_user_x_projects';
+    protected $table = 'relay_users_x_projects';
 
     /**
      * The attributes that are mass assignable.
--- database/migrations/2016_11_01_064219_create_relay_users_x_projects_table.php
+++ database/migrations/2016_11_01_064219_create_relay_users_x_projects_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRelayUserXProjectsTable extends Migration
+class CreateRelayUsersXProjectsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class CreateRelayUserXProjectsTable extends Migration
      */
     public function up()
     {
-        Schema::create('relay_user_x_projects', function (Blueprint $table) {
+        Schema::create('relay_users_x_projects', function (Blueprint $table) {
             $table->integer('user_id');
             $table->integer('project_id');
             $table->integer('authority');
@@ -31,6 +31,6 @@ class CreateRelayUserXProjectsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('relay_user_x_projects');
+        Schema::drop('relay_users_x_projects');
     }
 }
