b3bd9e6c7dd1c8c7f25303d636eae7b55763bee2
Tomoya Koyanagi <tomk79@gmail.com>
2016-11-01 17:22:03 +0900

    relay_user_x_projects 定義更新

--- app/relayUserXProjects.php
+++ app/relayUserXProjects.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class relayUserXProjects extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'relay_user_x_projects';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['user_id', 'project_id', 'authority'];
+
 }
--- database/migrations/2016_11_01_064219_create_relay_user_x_projects_table.php
+++ database/migrations/2016_11_01_064219_create_relay_user_x_projects_table.php
@@ -13,8 +13,14 @@ class CreateRelayUserXProjectsTable extends Migration
     public function up()
     {
         Schema::create('relay_user_x_projects', function (Blueprint $table) {
-            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('project_id');
+            $table->integer('authority');
             $table->timestamps();
+
+            // foreignkey制約
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('project_id')->references('id')->on('projects');
         });
     }
 
