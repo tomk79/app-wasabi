2550bdbce93f17188a24df034af8f0b3d6cee8bc
Tomoya Koyanagi <tomk79@gmail.com>
2019-06-02 03:10:02 +0900

    Home画面を仮置き

--- resources/views/startpage/dashboard.blade.php
+++ resources/views/startpage/dashboard.blade.php
@@ -1,8 +1,55 @@
 @extends('layouts.app')
 @section('title') {{ config('app.name') }} @endsection
 
+@section('head')
+<style>
+.cont-account-icon{
+	width: 100%;
+}
+</style>
+@endsection
+
 @section('content')
 <div class="container">
-	<p>ようこそ {{ config('app.name') }}</p>
+	<div class="row">
+		<div class="col-4">
+			<p><img src="{{{ $profile->icon }}}" class="account-icon cont-account-icon" /></p>
+		</div>
+		<div class="col">
+			<table class="table table__dd">
+				<tbody>
+					<tr>
+						<th>ユーザー名</th>
+						<td>{{{ $profile->name }}}</td>
+					</tr>
+					<tr>
+						<th>アカウント名</th>
+						<td>{{{ $profile->account }}}</td>
+					</tr>
+					<tr>
+						<th>パスワード</th>
+						<td>********</td>
+					</tr>
+				</tbody>
+			</table>
+			<div class="text-right">
+				<p><a href="{{ url('/settings/profile/edit') }}" class="btn btn-primary">プロフィールを編集する</a></p>
+			</div>
+
+			<table class="table table__dd">
+				<tbody>
+					<tr>
+						<th>メールアドレス</th>
+						<td>{{{ $profile->email }}}</td>
+					</tr>
+				</tbody>
+			</table>
+			<div class="text-right">
+				<p><a href="{{ url('/settings/profile/edit_email') }}" class="btn btn-primary">メールアドレスを変更する</a></p>
+			</div>
+
+		</div>
+	</div>
+
 </div>
 @endsection
