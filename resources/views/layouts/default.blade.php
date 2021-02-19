a5aa268299f77326dce3a935bfc9f86d24f7020d
Tomoya Koyanagi <tomk79@gmail.com>
2021-02-19 16:35:39 +0900

    テーマ改善 - タイトルがセットされていない場合の titleタグの出し分け

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,7 +5,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
+	@hasSection('title')
 	<title>{{ config('app.name') }} | @yield('title')</title>
+	@else
+	<title>{{ config('app.name') }}</title>
+	@endif
 
 	@include("layouts.inc.head")
 
